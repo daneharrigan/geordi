e2fb2947f78e2f81e7fcc0445dbbc42809e6fb5f
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-02 18:49:37 -0700

    saving scanner

--- scanner/scanner.go
+++ scanner/scanner.go
@@ -1,17 +1,45 @@
 package scanner
 
+import "errors"
+
+import "fmt"
+
 // https://gist.github.com/daneharrigan/dd51c0e02d156c0259ba
 
 type Scanner struct {
 	b []byte
 	e error
+	c bool
 	s int
 	f int
-	
+	i int
 }
 
+var (
+	ErrArgumentQuoting    = errors.New("incomplete argument quoting")
+	ErrUnexpectedCR       = errors.New(`unexpected \r character`)
+	ErrUnexpectedCmdChr   = errors.New("unexpected character in command name")
+	ErrUnexpectedEscaping = errors.New("unexpected escaping in argument")
+	ErrInvalidDataType    = errors.New("invalid data type")
+)
+
 func New(b []byte) *Scanner {
-	return &Scanner{b, nil, -1, -1}
+	return &Scanner{b: b, c: true}
+}
+
+func (s *Scanner) Scan() bool {
+	if s.c {
+		s.s, s.f, s.i, s.e = scanCommand(s.b)
+	} else {
+		s.s, s.f, s.i, s.e = scanArgument(s.b, s.i)
+	}
+
+	if s.e != nil {
+		return false
+	}
+
+	fmt.Printf("i=%d len=%d\n", s.i, len(s.b))
+	return s.i+1 == len(s.b)
 }
 
 func (s *Scanner) Err() error {
@@ -22,6 +50,163 @@ func (s *Scanner) Bytes() []byte {
 	return s.b[s.s:s.f]
 }
 
-func (s *Scanner) Scan() bool {
-	return false
+func scanCommand(b []byte) (int, int, int, error) {
+	s := -1
+	f := -1
+	i := 0
+
+	for n, c := range b {
+		i = n
+		switch {
+		case c == ' ' || c == '\t' || c == '\n':
+			fmt.Printf("s=%d f=%d\n", s, f)
+			if f > 0 {
+				return s, f, i, nil
+			}
+
+			continue
+		case c >= 65 && c <= 90:
+			if s < 0 {
+				s = i
+			}
+
+			f = i+1
+		case c == '\r':
+			return 0, 0, i, ErrUnexpectedCR
+		default:
+			return 0, 0, i, ErrUnexpectedCmdChr
+		}
+	}
+
+	return s, f, i, nil
 }
+
+func scanArgument(b []byte, i int) (int, int, int, error) {
+	var isQuoted, isEscaped bool
+	s := -1
+	f := -1
+
+	for _, c := range b {
+		i++
+		if isEscaped {
+			isEscaped = false
+			continue
+		}
+
+		switch {
+		case c == '"' && !isQuoted:
+			s = i
+			isQuoted = true
+		case c == '"' && isQuoted:
+			f = i
+			return s, f, i, nil
+		case c == '\\':
+			if !isQuoted {
+				return 0, 0, 0, ErrUnexpectedEscaping
+			}
+
+			isEscaped = true
+			continue
+		}
+	}
+
+	if isQuoted {
+		return 0, 0, 0, ErrArgumentQuoting
+	}
+
+	return s, f, i, nil
+}
+
+/*
+
+func (s *Scanner) Scan() bool {
+	var c byte
+	s.s = -1
+	s.f = -1
+whitespace:
+	if len(s.b) == s.i {
+		if s.q {
+			s.e = ErrArgumentQuoting
+			return false
+		}
+
+		if s.f < len(s.b) {
+			s.f = len(s.b)
+		fmt.Printf("bytes=%q\n", s.Bytes())
+			return false
+		}
+
+		fmt.Printf("bytes=%q\n", s.Bytes())
+		return false
+	} else {
+		c = s.b[s.i]
+	}
+
+	switch c {
+	case '\n', '\t', ' ':
+		if !s.q && s.f < 0 {
+			s.f = s.i
+		}
+
+		if s.f > s.s {
+			return true
+		}
+
+		s.i++
+		goto whitespace
+	case '\r':
+		s.e = ErrUnexpectedCR
+		return false
+	default:
+		if s.c {
+			goto command
+		}
+
+		goto argument
+	}  
+
+	return false
+command:
+	c = s.b[s.i]
+	switch {
+	case c == '\n' || c == '\t' || c == ' ':
+		s.f = s.i
+		s.c = false
+		goto whitespace
+	case c >= 65 && c <= 90:
+		if s.s < 0 {
+			s.s = s.i
+		}
+		s.i++
+		goto command
+	default:
+		s.e = ErrUnexpectedCmdChr
+		return false
+	}
+argument:
+	c = s.b[s.i]
+	switch {
+	case c == '"' && !s.q:
+		s.s = s.i+1
+		s.q = true
+	case c == '"' && s.q:
+		s.q = false
+		s.f = s.i
+	case c == '\\':
+		if !s.q {
+			s.e = ErrUnexpectedEscaping
+			return false
+		}
+
+		s.i+=2 // skip over escaped char
+		goto argument
+	default:
+		if s.s < 0 {
+			s.s = s.i
+		}
+	}
+
+	s.i++
+	goto whitespace
+}
+*/
--- scanner/scanner_test.go
+++ scanner/scanner_test.go
@@ -1,49 +1,78 @@
 package scanner_test
 
 import (
+	"bytes"
 	"github.com/daneharrigan/geordi/scanner"
 	"testing"
-	"bytes"
 )
 
 func TestScan(t *testing.T) {
-	b := []byte(`CMD "key" "value" 123 12.3 -1`)
-	s := scanner.New(b)
-	values := [][]byte{
-		[]byte("CMD"),
-		[]byte("key"),
-		[]byte("value"),
-		[]byte("123"),
-		[]byte("12.3"),
-		[]byte("-1"),
+	values := []struct {
+		b []byte
+		v [][]byte
+	}{
+		{[]byte("CMD"),           [][]byte{[]byte("CMD")}},
+		{[]byte(`CMD "key"`),     [][]byte{[]byte("CMD"), []byte("key")}},
+		{[]byte(`CMD "key" 1`),   [][]byte{[]byte("CMD"), []byte("key"), []byte("1")}},
+		{[]byte(`CMD "key" 1.0`), [][]byte{[]byte("CMD"), []byte("key"), []byte("1.0")}},
+		{[]byte(`CMD "key" -1`),  [][]byte{[]byte("CMD"), []byte("key"), []byte("-1")}},
+		{[]byte(`CMD "key" "value"`), [][]byte{[]byte("CMD"), []byte("key"), []byte("value")}},
+		{[]byte(`CMD "key" "value" 1 2.0 -3`), [][]byte{
+			[]byte("key"), []byte("value"), []byte("1"), []byte("2.0"), []byte("-3")},
+		},
 	}
 
-	var i int
-	for s.Scan() {
-		if !bytes.Equal(s.Bytes(), values[i]) {
-			t.Fatalf("expected %q; was %q", s.Bytes(), values[i])
+	for i, tt := range values {
+		s := scanner.New(tt.b)
+		for n := 0; n < len(tt.v); n++ {
+			incomplete := s.Scan()
+			if n+1 == len(tt.v) && incomplete {
+				t.Fatalf("at %d:%d expected %v; was %v", i, n, false, incomplete)
+			}
+
+			if !bytes.Equal(s.Bytes(), tt.v[n]) {
+				t.Fatalf("at %d:%d expected %q; was %q", i, n, tt.v[n], s.Bytes())
+			}
+
+			if s.Err() != nil {
+				t.Fatalf("at %d expected %v; was %q", i, nil, s.Err())
+			}
 		}
-		i++
-	}
-
-	if i < 6 {
-		t.Fatalf("expected %d; was %d", 6, i)
-	}
-
-	if s.Err() != nil {
-		t.Fatalf("expected %v; was %q", nil, s.Err())
 	}
 }
 
+/*
 func TestScanErrCmd(t *testing.T) {
-	b := []byte("CMD1")
-	s := scanner.New(b)
-	if s.Scan() {
-		t.Fatalf("expected %v; was %v", false, true)
+	values := []struct {
+		b []byte
+		e error
+	}{
+		{[]byte("CMD1"), scanner.ErrUnexpectedCmdChr},
+		{[]byte("cmd"), scanner.ErrUnexpectedCmdChr},
+		{[]byte("!CMD"), scanner.ErrUnexpectedCmdChr},
+		{[]byte(`"CMD"`), scanner.ErrUnexpectedCmdChr},
+
+		{[]byte("CMD foo\r"), scanner.ErrUnexpectedCR},
+		{[]byte("\rCMD foo"), scanner.ErrUnexpectedCR},
+		{[]byte("CMD\rfoo"), scanner.ErrUnexpectedCR},
+
+		{[]byte(`CMD "`), scanner.ErrArgumentQuoting},
+		{[]byte(`CMD "key\"`), scanner.ErrArgumentQuoting},
+		{[]byte("CMD key"), scanner.ErrArgumentQuoting},
+
+		{[]byte("\\CMD"), scanner.ErrUnexpectedEscaping},
+		{[]byte(`CMD \"key"`), scanner.ErrUnexpectedEscaping},
 	}
 
-	if s.Err() != scanner.ErrUnexpectedCmdChr {
-		t.Fatalf("expected %q; was %q", scanner.ErrUnexpectedCmdChr,
-			s.Err())
+	for i, tt := range values {
+		s := scanner.New(tt.b)
+		if s.Scan() {
+			t.Fatalf("at %d expected %v; was %v", i, false, true)
+		}
+
+		if s.Err() != tt.e {
+			t.Fatalf("at %d expected %q; was %q", i, tt.e, s.Err())
+		}
 	}
 }
+*/
