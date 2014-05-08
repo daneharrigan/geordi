cbd5773fe19b3d47f0ca8d4124b26dd6516542fd
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-08 15:14:53 -0700

    create store

---
+++ store/store.go
@@ -0,0 +1,37 @@
+package store
+
+import (
+	"github.com/daneharrigan/geordi/types"
+	"errors"
+)
+
+var (
+	store = make(map[string]*Record)
+	ErrNotFound = errors.New("record not found")
+)
+
+type Record struct {
+	Value interface{}
+	Type types.Type
+}
+
+func NewRecord(v interface{}, t types.Type) *Record {
+	return *Record{v, t}
+}
+
+func Set(k string, r *Record) {
+	store[k] = r
+}
+
+func Set(k string, r *Record) {
+	store[k] = r
+}
+
+func Get(k string) (r *Record, err error) {
+	r, ok := store[k]
+	if ok {
+		return r, nil
+	}
+
+	return nil, ErrNotFound
+}
