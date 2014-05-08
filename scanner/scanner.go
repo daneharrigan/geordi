299c5a7d4e402b98ee4a2d07e3dae3d58d665f81
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-08 12:01:03 -0700

    create scanner

---
+++ scanner/scanner.go
@@ -0,0 +1,172 @@
+package scanner
+
+import (
+	"errors"
+	"github.com/daneharrigan/geordi/types"
+)
+
+var (
+	ErrArgumentQuoting    = errors.New("incomplete argument quoting")
+	ErrUnexpectedCR       = errors.New(`unexpected \r character`)
+	ErrUnexpectedCmdChr   = errors.New("unexpected character in command name")
+	ErrUnexpectedEscaping = errors.New("unexpected escaping in argument")
+	ErrInvalidDecimal     = errors.New("invalid decimal placement")
+	ErrInvalidDataType    = errors.New("invalid data type")
+)
+
+type Scanner struct {
+	bytes []byte
+	cmd   bool
+	err   error
+	i     int
+	s     int
+	f     int
+	t     types.Type
+}
+
+func New(b []byte) *Scanner {
+	return &Scanner{bytes: b, cmd: true}
+}
+
+func (s *Scanner) Scan() bool {
+	if s.cmd {
+		s.command()
+		s.cmd = false
+	} else {
+		s.argument()
+	}
+
+	if s.err != nil || len(s.bytes) == s.i {
+		return false
+	}
+
+	return true
+}
+
+func (s *Scanner) Err() error {
+	return s.err
+}
+
+func (s *Scanner) Type() types.Type {
+	return s.t
+}
+
+func (s *Scanner) Bytes() []byte {
+	return s.bytes[s.s:s.f]
+}
+
+func (s *Scanner) command() {
+	s.t = types.String
+	s.s = -1
+	for _, c := range s.bytes {
+		s.i++
+		switch {
+		case c == ' ' || c == '\t' || c == '\n':
+			if s.s < 0 {
+				continue
+			}
+
+			s.f = s.i - 1
+			return
+		case c == '\r':
+			s.err = ErrUnexpectedCR
+			return
+		case c >= 'A' && c <= 'Z':
+			if s.s < 0 {
+				s.s = s.i - 1
+			}
+			continue
+		default:
+			s.err = ErrUnexpectedCmdChr
+			return
+		}
+	}
+
+	if s.f < s.i {
+		s.f = s.i
+	}
+}
+
+func (s *Scanner) argument() {
+	s.s = -1
+	s.t = -1
+
+	var quoted bool
+	var escaped bool
+
+	for s.i < len(s.bytes) {
+		c := s.bytes[s.i]
+		s.i++
+		if escaped {
+			escaped = false
+			continue
+		}
+
+		switch {
+		case c == ' ' || c == '\t' || c == '\n':
+			if s.f < s.i && !quoted {
+				s.f = s.i - 1
+			}
+
+			if s.s > 0 && !quoted {
+				return
+			}
+		case c == '\r' && !quoted:
+			s.err = ErrUnexpectedCR
+			return
+		case c == '"' && !quoted:
+			quoted = true
+			s.t = types.String
+			s.s = s.i
+			continue
+		case c == '"' && quoted:
+			quoted = false
+			s.f = s.i - 1
+			return
+		case c == '\\':
+			if !quoted {
+				s.err = ErrUnexpectedEscaping
+				return
+			}
+
+			escaped = true
+			continue
+		case (c >= '0' && c <= '9') || c == '.' || c == '-':
+			if quoted {
+				continue
+			}
+
+			if s.s < 0 {
+				s.s = s.i - 1
+			}
+
+			if c == '.' {
+				if s.t == types.Float {
+					s.err = ErrInvalidDecimal
+					return
+				}
+
+				s.t = types.Float
+				continue
+			}
+
+			if s.t != types.Float {
+				s.t = types.Int
+			}
+		default:
+			if !quoted {
+				s.err = ErrArgumentQuoting
+				return
+			}
+		}
+	}
+
+	if quoted {
+		s.err = ErrArgumentQuoting
+		return
+	}
+
+	if s.f < s.i {
+		s.f = s.i
+	}
+}
---
+++ scanner/scanner_test.go
@@ -0,0 +1,169 @@
+package scanner
+
+import (
+	"bytes"
+	"github.com/daneharrigan/geordi/types"
+	"testing"
+)
+
+func TestScan(t *testing.T) {
+	type value struct {
+		b []byte
+		t types.Type
+	}
+
+	table := []struct {
+		operation []byte
+		values    []value
+		size      int
+	}{
+		{
+			[]byte("CMD"),
+			[]value{{[]byte("CMD"), types.String}},
+			1,
+		},
+		{
+			[]byte(`CMD "key"`),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("key"), types.String},
+			},
+			2,
+		},
+		{
+			[]byte("CMD 1"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("1"), types.Int},
+			},
+			2,
+		},
+		{
+			[]byte("CMD .5"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte(".5"), types.Float},
+			},
+			2,
+		},
+		{
+			[]byte("CMD 0.5"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("0.5"), types.Float},
+			},
+			2,
+		},
+		{
+			[]byte("CMD -1"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("-1"), types.Int},
+			},
+			2,
+		},
+		{
+			[]byte(`CMD "key" "value" 1 -1 .5 0.5`),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("key"), types.String},
+				{[]byte("value"), types.String},
+				{[]byte("1"), types.Int},
+				{[]byte("-1"), types.Int},
+				{[]byte(".5"), types.Float},
+				{[]byte("0.5"), types.Float},
+			},
+			7,
+		},
+
+		{
+			[]byte(" CMD 1 "),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("1"), types.Int},
+			},
+			2,
+		},
+		{
+			[]byte("\tCMD\t1\t"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("1"), types.Int},
+			},
+			2,
+		},
+		{
+			[]byte("\nCMD\n1\n"),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("1"), types.Int},
+			},
+			2,
+		},
+		{
+			[]byte("\n\t CMD\n\t 1\n\t "),
+			[]value{
+				{[]byte("CMD"), types.String},
+				{[]byte("1"), types.Int},
+			},
+			2,
+		},
+	}
+
+	for _, tt := range table {
+		o := tt.operation
+		s := New(o)
+		var i int
+
+		for n := 0; n < len(tt.values); n++ {
+			s.Scan()
+			v := tt.values[n]
+			a := v.b
+			b := s.Bytes()
+			i++
+
+			if !bytes.Equal(a, b) {
+				t.Fatalf("from %q, expected %q; was %q", o, a, b)
+			}
+
+			if s.Type() != v.t {
+				t.Fatalf("from %q, expected %q; was %q", o,
+					s.Type(), v.t)
+			}
+		}
+
+		if s.Err() != nil {
+			t.Fatalf("expected %q; was nil", s.Err())
+		}
+
+		if i < tt.size {
+			t.Fatalf("expected %d; was %d", tt.size, i)
+		}
+
+	}
+}
+
+func TestError(t *testing.T) {
+	values := []struct {
+		operation []byte
+		err       error
+	}{
+		{[]byte(`"CMD"`), ErrUnexpectedCmdChr},
+		{[]byte(`"CMD1`), ErrUnexpectedCmdChr},
+		{[]byte(`"CMD!`), ErrUnexpectedCmdChr},
+		{[]byte("CMD \r"), ErrUnexpectedCR},
+		{[]byte(`CMD key`), ErrArgumentQuoting},
+		{[]byte(`CMD "`), ErrArgumentQuoting},
+		{[]byte(`CMD \"`), ErrUnexpectedEscaping},
+		{[]byte("CMD 1.1.1"), ErrInvalidDecimal},
+	}
+
+	for _, tt := range values {
+		s := New(tt.operation)
+		for s.Scan() {
+		}
+		if s.Err() != tt.err {
+			t.Fatalf("expected %q; was %e", tt.err, s.Err())
+		}
+	}
+}
