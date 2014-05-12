2035b8241e6c525d3eccaa20fa0c713ae34de2fc
Dane Harrigan <dane.harrigan@gmail.com>
2014-05-12 16:09:38 -0700

    create responder

---
+++ responder/responder.go
@@ -0,0 +1,41 @@
+package responder
+
+import (
+	"bytes"
+	"io"
+)
+
+type Responder struct {
+	bytes  []byte
+	writer io.Writer
+	buffer *bytes.Buffer
+}
+
+func NewResponder(writer io.Writer) *Responder {
+	responder := &Responder{writer: writer}
+	responder.buffer = bytes.NewBuffer(responder.bytes)
+	return responder
+}
+
+func (r *Responder) WriteError() {
+	r.buffer.WriteByte('-')
+}
+
+func (r *Responder) WriteSuccess() {
+	r.buffer.WriteByte('+')
+}
+
+func (r *Responder) Write(b []byte) {
+	r.buffer.Write(b)
+}
+
+func (r *Responder) WriteString(s string) {
+	r.buffer.WriteString(s)
+}
+
+func (r *Responder) Flush() error {
+	r.buffer.WriteByte('\r')
+	_, err := r.buffer.WriteTo(r.writer)
+	r.buffer.Reset()
+	return err
+}
